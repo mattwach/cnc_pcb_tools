e7a78eb9d0b1b2af105fcd3c5315644ca4b43ea0
mattwach <matt.wachowski@gmail.com>
2023-05-01 09:00:08 -0700

    Added stencil check

--- check_pcb_common.py
+++ check_pcb_common.py
@@ -1,13 +1,13 @@
 """Common checks that would go in every file."""
 
-def check_common(check):
+def check_common(check, maxx=100.0, miny=-70.0):
   """Common checks that would otherwise have to be repeated in every file."""
   check.dump_properties()
   check.assert_spinning()
   # Assumes a 7x10cm board with the origin at the upper-left
   check.assert_gt('min_x', check.min_x(), -2.0)
-  check.assert_lt('max_x', check.max_x(), 100.0)
-  check.assert_gt('min_y', check.min_y(), -70.0)
+  check.assert_lt('max_x', check.max_x(), maxx)
+  check.assert_gt('min_y', check.min_y(), miny)
   check.assert_lt('max_y', check.max_y(), 2.0)
 
   # maximum allowed z
